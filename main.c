743f8ad3a782bb7c768bc95f6ecdfbaa467c73aa
Ahuitzolt <emmanuel.noguez@gmail.com>
2020-07-29 23:11:19 -0500

    Made blinky LED.

--- main.c
+++ main.c
@@ -3,17 +3,42 @@
 // 0x4002.5000, 0x51C, DEN
 // 0x4002.5000, 0x000, DATA
 
+	int delay;
+
+
 int main()
 {
 	unsigned int *pt;
+	
 	pt = (unsigned int *) 0x400FE608U;
 	*pt = 0x20U; 	// 0b 0000 0000 0000 0000 0000 0000 0010 0000 0x00000020 is equal to 0x20
 	pt = (unsigned int *) 0x40025400U;
 	*pt = 0x0EU;							// 0b 0000 0000 0000 0000 0000 0000 0000 1110 
 	pt = (unsigned int *) 0x4002551CU;
 	*pt = 0x0EU;							// 0b 0000 0000 0000 0000 0000 0000 0000 1110 
-	pt = (unsigned int *) 0x400253FCU ; // 0x400253FCU to eneable all data register
-	*pt = 0x0AU; // color
+	
+	while(1)
+	{
+	delay = 0;
+	while(delay < 1000000)
+	{
+		pt = (unsigned int *) 0x400253FCU; // 0x400253FCU to eneable all data register
+		*pt = 0x0AU; // color
+		++delay;
+	}
+	
+	delay = 0;
+	
+		while(delay < 1000000)
+	{
+		pt = (unsigned int *) 0x400253FCU;
+		*pt = 0x00U;
+		++delay;
+	}
+	}
+
+	
+	
 	/*
 	0x02U  Red
 	0x04U  Blue
