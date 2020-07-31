8ed77391eb4628a181647df93ea0133dd7593d8d
Ahuitzolt <emmanuel.noguez@gmail.com>
2020-07-31 00:33:46 -0500

    Improved code with preprocessors.

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 // 0x4002.5000, 0x51C, DEN
 // 0x4002.5000, 0x000, DATA
 
+#define CLOCK_REGISTER 				(*((unsigned int *) 0x400FE608U))
+#define DIRECTION_REGISTER		(*((unsigned int *) 0x40025400U))
+#define ENABLE_REGISTER				(*((unsigned int *) 0x4002551CU))
+#define DATA_REGISTER					(*((unsigned int *) 0x400253FCU))
+
 	int delay;
 
 
@@ -10,20 +15,16 @@ int main()
 {
 	unsigned int *pt;
 	
-	pt = (unsigned int *) 0x400FE608U;
-	*pt = 0x20U; 	// 0b 0000 0000 0000 0000 0000 0000 0010 0000 0x00000020 is equal to 0x20
-	pt = (unsigned int *) 0x40025400U;
-	*pt = 0x0EU;							// 0b 0000 0000 0000 0000 0000 0000 0000 1110 
-	pt = (unsigned int *) 0x4002551CU;
-	*pt = 0x0EU;							// 0b 0000 0000 0000 0000 0000 0000 0000 1110 
+	CLOCK_REGISTER = 0x20U; 	// 0b 0000 0000 0000 0000 0000 0000 0010 0000 0x00000020 is equal to 0x20
+	DIRECTION_REGISTER = 0x0EU;							// 0b 0000 0000 0000 0000 0000 0000 0000 1110 
+	ENABLE_REGISTER = 0x0EU;							// 0b 0000 0000 0000 0000 0000 0000 0000 1110 
 	
 	while(1)
 	{
 	delay = 0;
 	while(delay < 1000000)
 	{
-		pt = (unsigned int *) 0x400253FCU; // 0x400253FCU to eneable all data register
-		*pt = 0x0AU; // color
+		DATA_REGISTER = 0X0CU; // color
 		++delay;
 	}
 	
@@ -31,29 +32,19 @@ int main()
 	
 		while(delay < 1000000)
 	{
-		pt = (unsigned int *) 0x400253FCU;
-		*pt = 0x00U;
+		DATA_REGISTER = 0x00U;
 		++delay;
 	}
 	}
 
-	
-	
 	/*
 	0x02U  Red
 	0x04U  Blue
 	0x08U  Green
 	0x0EU  White
-	0x0AU	 Amarillo
+	0x0AU	 Yellow
+	0X0CU	 CYAN
 		1 			1				1 			0
 	green		Blue		Red
 	*/
-	
-	
-	
-	
 }
-
-
-
-
