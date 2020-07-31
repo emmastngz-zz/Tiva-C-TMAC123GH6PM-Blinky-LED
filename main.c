f162ede894a3745b2c6b506ae2935280f92e47c3
Ahuitzolt <emmanuel.noguez@gmail.com>
2020-07-31 00:51:18 -0500

    Added header file.

--- main.c
+++ main.c
@@ -1,30 +1,21 @@
-// 0x400F.E000, 0x608, RCGC
-// 0x4002.5000, 0x400, DIR
-// 0x4002.5000, 0x51C, DEN
-// 0x4002.5000, 0x000, DATA
+#include "tm4c123gh6pm.h"
 
-#define RCGCGPIO 				(*((unsigned int *) 0x400FE608U))
-#define GPIOF_DIR		(*((unsigned int *) 0x40025400U))
-#define GPIOF_DEN				(*((unsigned int *) 0x4002551CU))
-#define GPIOF_DATA					(*((unsigned int *) 0x400253FCU))
-
-	int delay;
+int delay;
 
 
 int main()
 {
-	unsigned int *pt;
 	
-	RCGCGPIO = 0x20U; 	// 0b 0000 0000 0000 0000 0000 0000 0010 0000 0x00000020 is equal to 0x20
-	GPIOF_DIR = 0x0EU;							// 0b 0000 0000 0000 0000 0000 0000 0000 1110 
-	GPIOF_DEN = 0x0EU;							// 0b 0000 0000 0000 0000 0000 0000 0000 1110 
+	SYSCTL_RCGCGPIO_R   = 	0x20U; 	// 0b 0000 0000 0000 0000 0000 0000 0010 0000 0x00000020 is equal to 0x20
+	GPIO_PORTF_DIR_R    = 	0x0EU;							// 0b 0000 0000 0000 0000 0000 0000 0000 1110 
+	GPIO_PORTF_DEN_R 		= 	0x0EU;							// 0b 0000 0000 0000 0000 0000 0000 0000 1110 
 	
 	while(1)
 	{
 	delay = 0;
 	while(delay < 1000000)
 	{
-		GPIOF_DATA = 0X0CU; // color
+		GPIO_PORTF_DATA_R  = 0X0CU; // color
 		++delay;
 	}
 	
@@ -32,7 +23,7 @@ int main()
 	
 		while(delay < 1000000)
 	{
-		GPIOF_DATA = 0x00U;
+		GPIO_PORTF_DATA_R  = 0x00U;
 		++delay;
 	}
 	}
